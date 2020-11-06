8a94222aa6ebe76c1129930699e28b1109c2039b
MukheshRavi <mukeshparvathi@gmail.com>
2020-11-06 19:06:35 +0530

    welcome message added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Payroll_Service_Ado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Payroll Service");
+        }
+    }
+}
