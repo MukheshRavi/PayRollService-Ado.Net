8be2570f17e7d51e5958a3e99419f51dc54e9268
MukheshRavi <mukeshparvathi@gmail.com>
2020-11-07 13:17:20 +0530

    [Mukhesh].Add Implemented Database functions

--- PayrollRepository.cs
+++ PayrollRepository.cs
@@ -135,8 +135,8 @@ Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubn
                         employeeDetails.empName = Convert.ToString(reader[1]);
                         employeeDetails.gender = Convert.ToChar(reader[2]);
                         employeeDetails.phoneNumber = Convert.ToString(reader[3]);
-                        employeeDetails.payrollId = Convert.ToInt32(reader[4]);
-                        employeeDetails.startDate = Convert.ToDateTime(reader[5]);
+                        employeeDetails.payrollId = Convert.ToInt32(reader[5]);
+                        employeeDetails.startDate = Convert.ToDateTime(reader[4]);
                         Console.WriteLine(employeeDetails.empId + "       " + employeeDetails.empName + "        " +
                                            employeeDetails.gender + "         " + employeeDetails.phoneNumber + "         " +
                                            employeeDetails.payrollId + "          " + employeeDetails.startDate);
@@ -154,6 +154,47 @@ Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubn
             finally
             {
                     connection.Close();
+            }
+        }
+        public void ImplementDatabaseFunctions()
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    string query = @"select e.Gender ,min(p.BasePay-p.Deductions-p.TaxablePay)as MinNetPay,
+                                                      max(p.BasePay-p.Deductions-p.TaxablePay)as MaxNetPay,
+                                                      sum(p.BasePay-p.Deductions-p.TaxablePay)as SumNetPay,
+                                                      Avg(p.BasePay-p.Deductions-p.TaxablePay)as AvgNetPay,count(e.gender) as Count
+                                   from PayrollDetails p inner join Employee e on e.PayrollId=p.payrollId group by e.Gender";
+                    SqlCommand command = new SqlCommand(query, this.connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            string gender = reader[0].ToString();
+                            double minNetPay = reader.GetInt32(1);
+                            double maxNetPay= reader.GetInt32(2);
+                            double sumNetPay = reader.GetInt32(3);
+                            double avgNetPay = reader.GetInt32(4);
+                            int count = reader.GetInt32(5);
+                            Console.WriteLine("Gender:{0}\tCount:{1}\tMinSalary:{2}\tMaxSalary:{3}\tSumOfSalary:{4}\tAvgSalary:{5}\n", gender, count, minNetPay, 
+                                maxNetPay, sumNetPay, avgNetPay);
+                        }
+                    }
+                    else
+                        Console.WriteLine("No data found");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ namespace Payroll_Service_Ado
         {
             PayrollRepository payrollRepository = new PayrollRepository();
             Console.WriteLine("Welcome to Payroll Service");
-           // payrollRepository.GetPayrollDetails();
-           // payrollRepository.UpdateSalary();
-            payrollRepository.RetrieveWithStartDate();
+            // payrollRepository.GetPayrollDetails();
+            // payrollRepository.UpdateSalary();
+            //payrollRepository.RetrieveWithStartDate();
+            payrollRepository.ImplementDatabaseFunctions();
 
         }
     }
